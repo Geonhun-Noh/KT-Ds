04ae4fdc1d699395d46e1b1c552411453086bd3a
Geonhun-Noh <53288283+Geonhun-Noh@users.noreply.github.com>
2020-06-18 11:10:12 +0900

    Add files via upload

---
+++ JavaEx/src/JavaEx/CelToFah/src/Average.java
@@ -0,0 +1,18 @@
+/*
+ * 실습예제2
+ * 2020.02.18
+ * 노건훈
+ */
+public class Average {
+	public static void main(String[] args) {
+		int math = 87;
+		int pyh = 46;
+		int eng = 83;
+		int prog = 100;
+		
+		double avg = (math + pyh + eng + prog)/4;
+		
+		System.out.println(avg);
+	}
+
+}
---
+++ JavaEx/src/JavaEx/CelToFah/src/CeltoFah.java
@@ -0,0 +1,15 @@
+/*
+ * 실습예제1
+ * 2020.02.18
+ * 노건훈
+ */
+
+public class CeltoFah {
+	public static void main(String[] args) {
+		double cel = -8;
+		double fah = (cel*9/5)+32;
+		
+		System.out.println(fah + "F");
+	}
+
+}
